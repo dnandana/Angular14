842461966cc0aadaa74a28d40dcbbbcefa5efed6
unknown <dnandana@gmail.com>
2022-08-03 13:06:28 +0530

    Deleted files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { StudentsComponent } from './students/students.component';
-import { Table1Component } from './table1/table1.component';
 
 
 const routes: Routes = [
-  {path:"students",component:StudentsComponent},
-  {path:"table1",component:Table1Component},
-  {path:"login",component:LoginComponent},
 
 ];
   @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentsComponent } from './students/students.component';
-import { Table1Component } from './table1/table1.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent,
-    Table1Component,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
